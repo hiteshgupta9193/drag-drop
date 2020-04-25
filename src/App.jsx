b3a2ec503118da7d67829d7202cdfe9f49d73694
Hitesh Gupta <hitesh.gupta@proptiger.com>
2020-04-25 19:30:22 +0530

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.scss'
+import { container } from './appStyle'
+
+const App = () => (
+  <div css={container}>
+    <h1>Hello World, React!</h1>
+  </div>
+)
+
+export default App;
